1997fe2844736d6f778c40f553dc89dbaebd7f29
amanriyas <153854025+amanriyas@users.noreply.github.com>
2025-06-09 14:59:10 +0400

    more changes

--- fastdock/settings.py
+++ fastdock/settings.py
@@ -11,6 +11,14 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
+import os
+from dotenv import load_dotenv
+
+
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +45,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    
 ]
 
 MIDDLEWARE = [
